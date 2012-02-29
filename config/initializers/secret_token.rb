cd3c6f3bf3985723ba1fca2768d5ec723111b3cd
Simo Peterson <simo102@hot.ee>
2012-02-29 13:31:49 +0200

    new project started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RorCompanyPage126::Application.config.secret_token = '315fd64d10f208d2bec83aaf1d8a23762cc40c26ea4affce40628ce3615d7245e2c2f27931526dfb19cce35bc247c173e3d7f099308de83a59b77863cb3b4d24'
